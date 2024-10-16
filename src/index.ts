51490082a0474b982d0967fd61d0a1d115ae2e98
zackyantoleo <zackyanto.leo@gmail.com>
2024-10-16 23:26:17 +0700

    Add order processing endpoints

--- src/index.ts
+++ src/index.ts
@@ -35,3 +35,16 @@ app.get("/products", (req: Request, res: Response) => {
   // Logika untuk mendapatkan semua produk
   res.send("List of products");
 });
+
+// Endpoint untuk membuat order
+app.post("/orders", (req: Request, res: Response) => {
+  const { productId, quantity } = req.body;
+  // Logika untuk membuat order
+  res.send("Order created");
+});
+
+// Endpoint untuk mendapatkan semua order
+app.get("/orders", (req: Request, res: Response) => {
+  // Logika untuk mendapatkan semua order
+  res.send("List of orders");
+});
